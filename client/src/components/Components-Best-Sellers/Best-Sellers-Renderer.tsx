b1bdb2ac727b3cd82ca9e23a37a6bce253fc2d48
velichetyvamsikrishna <vamsi.velichety@gmail.com>
2024-04-21 16:35:33 -0700

    Update Best-Sellers-Renderer.tsx

--- client/src/components/Components-Best-Sellers/Best-Sellers-Renderer.tsx
+++ client/src/components/Components-Best-Sellers/Best-Sellers-Renderer.tsx
@@ -1,51 +1,16 @@
 import React from "react";
-import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 import {
   Typography,
   Card,
   CardContent,
   IconButton,
   Grid,
+  Container
 } from "@material-ui/core";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
 import GroceryItemCardRenderer from "../Grocery-Item-Card/Grocery-Item-Card-Renderer";
-
-// Define styles for the component
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    mainTitle: {
-      textAlign: "center",
-      marginBottom: theme.spacing(4),
-    },
-    cardContainer: {
-      position: "relative",
-    },
-    card: {
-      minWidth: 275,
-      textAlign: "center",
-      height: "100%",
-      transition: "transform 0.3s ease-in-out",
-    },
-    arrowsContainer: {
-      display: "flex",
-      justifyContent: "space-between",
-      alignItems: "center",
-      marginTop: theme.spacing(2),
-    },
-    arrowButton: {
-      backgroundColor: "rgba(255, 255, 255, 0.5)",
-      borderRadius: "50%",
-      padding: theme.spacing(1),
-      "&:hover": {
-        backgroundColor: "rgba(255, 255, 255, 0.8)",
-      },
-    },
-  })
-);
+import { useStyles } from "./Best-Sellers.styles";
 
 const BestSellersRenderer: React.FC = () => {
   const classes = useStyles();
@@ -61,38 +26,40 @@ const BestSellersRenderer: React.FC = () => {
   };
 
   return (
-    <div className={classes.root}>
-      <Typography variant="h4" className={classes.mainTitle}>
-        Best Sellers
-      </Typography>
-      <div className={classes.cardContainer}>
-        <Grid container spacing={3}>
-          {cards.slice(currentIndex, currentIndex + 4).map((card, index) => (
-            <Grid key={index} item xs={12} sm={3}>
-              <GroceryItemCardRenderer />
-            </Grid>
-          ))}
-        </Grid>
-        {cards.length > 4 && (
-          <div className={classes.arrowsContainer}>
-            <IconButton
-              className={classes.arrowButton}
-              onClick={handlePrev}
-              disabled={currentIndex === 0}
-            >
-              <ArrowBackIcon />
-            </IconButton>
-            <IconButton
-              className={classes.arrowButton}
-              onClick={handleNext}
-              disabled={currentIndex === cards.length - 4}
-            >
-              <ArrowForwardIcon />
-            </IconButton>
-          </div>
-        )}
+    <Container maxWidth="false" >
+      <div className={classes.root}>
+        <Typography variant="h4" className={classes.mainTitle}>
+          Best Sellers
+        </Typography>
+        <div className={classes.cardContainer}>
+          <Grid container spacing={4}>
+            {cards.slice(currentIndex, currentIndex + 4).map((card, index) => (
+              <Grid key={index} item xs={12} sm={9} md={6} lg={3}>
+                <GroceryItemCardRenderer />
+              </Grid>
+            ))}
+          </Grid>
+          {cards.length > 4 && (
+            <div className={classes.arrowsContainer}>
+              <IconButton
+                className={classes.arrowButton}
+                onClick={handlePrev}
+                disabled={currentIndex === 0}
+              >
+                <ArrowBackIcon />
+              </IconButton>
+              <IconButton
+                className={classes.arrowButton}
+                onClick={handleNext}
+                disabled={currentIndex === cards.length - 4}
+              >
+                <ArrowForwardIcon />
+              </IconButton>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </Container>
   );
 };
 
