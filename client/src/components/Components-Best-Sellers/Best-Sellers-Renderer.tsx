564a413cd6351439ea9fd35cd3eb8d56a69ab51a
velichetyvamsikrishna <vamsi.velichety@gmail.com>
2024-06-08 23:24:59 -0700

    Update Best-Sellers-Renderer.tsx

--- client/src/components/Components-Best-Sellers/Best-Sellers-Renderer.tsx
+++ client/src/components/Components-Best-Sellers/Best-Sellers-Renderer.tsx
@@ -5,7 +5,7 @@ import {
   CardContent,
   IconButton,
   Grid,
-  Container
+  Container,
 } from "@material-ui/core";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
@@ -14,37 +14,19 @@ import { useStyles } from "./Best-Sellers.styles";
 //api
 import { useGetProductsByFilterAPI } from "../../api/productsAPI";
 
-
 const BestSellersRenderer: React.FC = () => {
   const classes = useStyles();
-  // const cards = [1, 2, 3, 4, 5, 6]; 
-  const [cards,setCards]=useState<any[]>([]);// products
-  const cardRef = useRef<HTMLDivElement>(null);
+  const [cards, setCards] = useState<any[]>([]); // products
+  //  const cardRef = useRef<HTMLDivElement>(null);
   const [numCardsToShow, setNumCardsToShow] = useState(4); // Initial number of cards to show
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
-    const handleResize = () => {
-      if (cardRef.current) {
-        const cardWidth = 275; // Width of each card
-        const cardsWidth = cardRef.current.offsetWidth;
-        const containerWidth = cardsWidth - 300; // Subtracting 100px padding from both sides
-        console.log("Container Width:", containerWidth);
-        const newNumCardsToShow = Math.floor(containerWidth / cardWidth);
-        setNumCardsToShow(Math.max(newNumCardsToShow, 1)); // Ensure at least 1 card is shown
-      }
-    };
-    window.addEventListener("resize", handleResize);
-    handleResize(); // Call the function initially
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-  useEffect(()=>{
     updateBestSellers();
-  },[]);
-  const updateBestSellers=async ()=>{
-    setCards(await useGetProductsByFilterAPI({"filter":"Bestsellers"}));
-  }
-  
+  }, []);
+  const updateBestSellers = async () => {
+    setCards(await useGetProductsByFilterAPI({ filter: "Bestsellers" }));
+  };
 
   const handlePrev = () => {
     setCurrentIndex((prevIndex) => Math.max(0, prevIndex - numCardsToShow));
@@ -57,45 +39,49 @@ const BestSellersRenderer: React.FC = () => {
   };
 
   return (
-    <div style={{ padding: "100px" }}>
-      {/* <Container> */}
-        <div className={classes.root}>
-          <Typography variant="h4" className={classes.mainTitle}>
-            Best Sellers
-          </Typography>
-          <div className={classes.cardContainer}>
-            <IconButton
-              className={`${classes.arrowButton} ${classes.leftArrow}`}
-              onClick={handlePrev}
-              disabled={currentIndex === 0}
-            >
-              <ChevronLeftIcon fontSize="large" />
-            </IconButton>
-            <Grid
-              container
-              spacing={4}
-              className={classes.gridContainer}
-              ref={cardRef}
-            >
+    <>
+      <div style={{display: 'flex'}}>
+        <IconButton
+          className={`${classes.arrowButton} ${classes.leftArrow}`}
+          onClick={handlePrev}
+          disabled={currentIndex === 0}
+        >
+          <ChevronLeftIcon fontSize="large" />
+        </IconButton>
+        <Container maxWidth="lg">
+          <div className={classes.root}>
+            <Typography variant="h4" className={classes.mainTitle} gutterBottom>
+              Best Sellers
+            </Typography>
+
+            <Grid container spacing={8}>
               {cards
                 .slice(currentIndex, currentIndex + numCardsToShow)
                 .map((card, index) => (
-                  <Grid key={index} item >
-                    <GroceryItemCardRenderer product={card}/>
+                  <Grid
+                    item
+                    key={index}
+                    xs={12}
+                    sm={9}
+                    md={6}
+                    lg={3}
+                    style={{ cursor: "pointer" }}
+                  >
+                    <GroceryItemCardRenderer product={card} />
                   </Grid>
                 ))}
             </Grid>
-            <IconButton
-              className={`${classes.arrowButton}`}
-              onClick={handleNext}
-              disabled={currentIndex === cards.length - numCardsToShow}
-            >
-              <ChevronRightIcon fontSize="large" />
-            </IconButton>
-          </div>
-        </div>
-      {/* </Container> */}
-    </div>
+          </div>  
+        </Container>
+        <IconButton
+          className={`${classes.arrowButton}`}
+          onClick={handleNext}
+          disabled={cards.length - numCardsToShow <=0 || currentIndex === cards.length - numCardsToShow}
+        >
+          <ChevronRightIcon fontSize="large" />
+        </IconButton>
+      </div>
+    </>
   );
 };
 
